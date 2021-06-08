3030ce51e24b1fb555f91f975adc34aecb494772
AeroScale <74071438+AeroScale@users.noreply.github.com>
2021-06-08 14:11:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # Telegram token
-token = '1474739385:AAFfathTMGg3xejLdd7nHtBmqLDf_s0eixc'
+token = ''
 
 APP_NAME = 'weatherbot_cwc'
 
 # PYOWM token
-token2 = '5032984dad902b5bbd29cc6d81ed2a8d'
+token2 = ''
 
 # Privatbank API
 url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
 
 #climacell Api
-key = "Riqd7NtECY2nJ47m9Vo47OwTrCcIoyh4"
+key = ""
